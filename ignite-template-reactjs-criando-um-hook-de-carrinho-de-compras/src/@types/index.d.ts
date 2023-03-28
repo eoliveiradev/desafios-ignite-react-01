a9695a7f5347e289f6b682b0ac619cb9f787ebbb
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-03-27 21:50:06 -0300

    Desafio 03 - Iniciando desafio

---
+++ ignite-template-reactjs-criando-um-hook-de-carrinho-de-compras/src/@types/index.d.ts
@@ -0,0 +1,27 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  amount: number;
+}
+
+export interface Stock {
+  id: number;
+  amount: number;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+export interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
+
+export interface CartItemsAmount {
+  [key: number]: number;
+}
