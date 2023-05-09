8ac3a1e9195b63e7259c062bbcfa2b140ca77dc9
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-05-09 19:53:39 -0300

    :sparkles: feat: PrismicProvider

--- ignite-template-reactjs-criando-um-projeto-do-zero/src/pages/_app.tsx
+++ ignite-template-reactjs-criando-um-projeto-do-zero/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
+import { PrismicProvider } from '@prismicio/react';
 import { DefaultLayout } from '../layouts/DefaultLayout';
+import { getPrismicClient } from '../services/prismic';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const prismic = getPrismicClient({});
+
   return (
     <DefaultLayout>
-      <Component {...pageProps} />;
+      <PrismicProvider client={prismic}>
+        <Component {...pageProps} />
+      </PrismicProvider>
     </DefaultLayout>
   );
 }
