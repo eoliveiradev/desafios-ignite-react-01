f9e182a8fd82f3953d49277e3341a62c38c5c539
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-05-09 19:52:50 -0300

    :sparkles: feat: Link para /post/uid

--- ignite-template-reactjs-criando-um-projeto-do-zero/src/components/Post/index.tsx
+++ ignite-template-reactjs-criando-um-projeto-do-zero/src/components/Post/index.tsx
@@ -1,9 +1,11 @@
 import { FaUser, FaCalendar } from 'react-icons/fa';
+import Link from 'next/link';
 import styles from './post.module.scss';
 import commonStyles from '../../styles/common.module.scss';
 import { Info } from '../Info';
 
 interface PostProps {
+  uid: string;
   title: string;
   description: string;
   date: string;
@@ -12,17 +14,21 @@ interface PostProps {
 
 export const Post = (props: PostProps): JSX.Element => {
   const {
-    title, date, description, user,
+    title, date, description, user, uid,
   } = props;
 
   return (
-    <div className={styles.container}>
-      <h1 className={commonStyles.heading}>{title}</h1>
-      <p className={commonStyles.paragraph}>{description}</p>
-      <footer>
-        <Info icon={FaUser} title={user} />
-        <Info icon={FaCalendar} title={date} />
-      </footer>
-    </div>
+    <Link href={`/post/${uid}`}>
+      <a>
+        <div className={styles.container}>
+          <h1 className={commonStyles.heading}>{title}</h1>
+          <p className={commonStyles.paragraph}>{description}</p>
+          <footer>
+            <Info icon={FaUser} title={user} />
+            <Info icon={FaCalendar} title={date} />
+          </footer>
+        </div>
+      </a>
+    </Link>
   );
 };
