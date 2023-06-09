494142fcad685d1325094a5b42adea8a6cde2c0a
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-06-09 11:53:37 -0300

    :label: chore: Tipagens

---
+++ ignite-interface-com-chakra-ui/src/@types/interfaces/index.d.ts
@@ -0,0 +1,26 @@
+interface ICity {
+  name: string;
+  image_url: string;
+  country: {
+    name: string;
+    flag_url: string;
+  }
+}
+
+export interface IContinent {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  short_description: string;
+  countries: {
+    total: number
+  };
+  cities: {
+    total: number;
+    data: ICity[];
+  };
+  languages: {
+    total: number;
+  }
+}
