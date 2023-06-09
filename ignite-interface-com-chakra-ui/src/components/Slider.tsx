3240808f0e7b3320cad5c741894f6100bede7de4
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-06-09 11:56:44 -0300

    :sparkles: feat: Slider

---
+++ ignite-interface-com-chakra-ui/src/components/Slider.tsx
@@ -0,0 +1,40 @@
+"use client";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "@/styles/swiper.css";
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, } from 'swiper'
+import { ReactNode } from 'react'
+
+interface SliderProps {
+  children: ReactNode[]
+  width: string;
+  height: string;
+}
+
+export const Slider = (props: SliderProps) => {
+  const { children, height, width } = props
+
+  return (
+    <Swiper
+      navigation={true}
+      modules={[Navigation, Pagination]}
+      pagination={{ clickable: true, }}
+
+      style={{
+        width,
+        height,
+      }}
+    >
+      {
+        children.map((slide, index) => (
+          <SwiperSlide key={index}>
+            {slide}
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  )
+}
