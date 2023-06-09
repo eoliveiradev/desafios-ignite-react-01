db52e73fc4e00e90d1aa7d54eb6091062478ae03
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2023-06-09 11:58:32 -0300

    :sparkles: feat: BackButton

---
+++ ignite-interface-com-chakra-ui/src/components/buttons/BackButton.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Button } from "@chakra-ui/react"
+import { useRouter } from "next/navigation"
+import { CaretLeft } from "phosphor-react"
+import { useEffect, useState } from "react";
+
+export const BackButton = () => {
+  const { back } = useRouter()
+
+  return (
+    <Button
+      variant="ghost"
+      onClick={back}
+    >
+
+      <CaretLeft size={26} color="#47585B" weight="bold" />
+    </Button>
+  )
+}
